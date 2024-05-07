4b37a9dc9ad4f9f3a2f0a4a614a477ea9a775e9e
navdeep <24gill@gmail.com>
2024-05-07 13:30:30 +0530

    first commit

---
+++ src/GP/Days.java
@@ -0,0 +1,11 @@
+package GP;
+
+public class Days {
+
+	public static void main(String[] args) {
+		String[] days= {"Sun","Mon","Tue"};
+		System.out.println(days[0]);
+
+	}
+
+}
---
+++ src/GP/Weeks.java
@@ -0,0 +1,11 @@
+package GP;
+
+public class Weeks {
+
+	public static void main(String[] args) {
+		int[] week= {1,2,3,4};
+		System.out.println(week[1]);
+
+	}
+
+}
